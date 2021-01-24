b1ad7ee1479f784627bcaef5101dca818e7892e0
cucumberhands <cyrus.simons@yahoo.com>
2021-01-24 13:14:52 -0500

    homepage updated, static files configured

--- Home/views.py
+++ Home/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Zipcode
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the Home index.")
+    zipcode_list = Zipcode.objects.order_by('entered')[:5]
+    template = loader.get_template('Home/index.html')
+    context = {
+        'zipcode_list': zipcode_list,
+    }
+    return HttpResponse(template.render(context, request))
+
 
 def search(request, zipcode_id):
     response = "Zipcode searched: %s."
